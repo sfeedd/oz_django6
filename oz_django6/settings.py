b3d9caeb83773f03ff207d8bde0e2fa82db9cde0
sfeedd <qowjdgus86@naver.com>
2024-11-13 11:19:26 +0900

    add black

--- oz_django6/settings.py
+++ oz_django6/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-c16ym8wwt$kwbg1(uud=p=l-wx8cj^g_*s*hzzzx&im%8&wr%%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
